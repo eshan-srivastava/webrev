6f6430351f09368d381b31c1e597a87923de76f1
Eshan Srivastava <eshan,srivastava273@gmail.com>
2022-06-03 22:34:10 +0530

    image slider completed

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-
-
-/*preloader logic
-var loader = doucment.getElementById("preloader");
-window.addEventListener("load", function(){
+//preloader logic
+let loader = document.getElementById("preloader");
+//window.addEventListener("load", function(){
+window.onload = () => {
     loader.style.display = "none";
-})*/
+    loader.remove();
+};
+
+let imgbase = document.querySelector(".img-box");
+let imgwrap = document.querySelector(".img-wrap");
+let wrap = document.getElementById("wrap");
+let arrowline = document.getElementById("arrow-span")
+
+wrap.style.height = imgbase.offsetHeight + "px";
+
+imgbase.onmousemove = function(elem){
+    let boxwidth = (elem.pageX - imgbase.offsetLeft) + "px";
+    imgwrap.style.width = boxwidth;
+    arrowline.style.left = boxwidth;
+
+}
